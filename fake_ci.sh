ece393a2f9f8369f6aa8ffbdaa30af2c9c9a133a
Meng Demao <mengdemao19951021@163.com>
2021-05-16 18:02:48 +0800

    Add fake_ci

---
+++ fake_ci.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# 设置版本号
+VERSION_MAJOR=$(git show -s --pretty=format:%h)
+VERSION_MINOR=$(git show -s --pretty=format:%h)
+
+# 启动构建
+docker build -t mengdemao/bytebox-compiler:VERSION_MAJOR.VERSION_MINOR .
+
